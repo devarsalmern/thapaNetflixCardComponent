6131f94e020ddaef3bb85bfdf0fc185c33227ed2
devarsalmern <hangergadget@gmail.com>
2024-08-20 20:37:07 +0500

    Netflix card component only

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+const Card = () => {
+  return (
+    <>
+      <div>
+        <img src="Queen of Tears.png" alt="" />
+      </div>
+      <h2>Name: "Queen of Tears"</h2>
+      <h3>Rating: 8.2</h3>
+      <p>
+        Summary: Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Blanditiis earum atque quod culpa sequi modi, odit laboriosam. Quos nisi
+        provident, natus repellendus eaque error, voluptatum, fuga quibusdam
+        similique veniam quia.
+      </p>
+    </>
+  );
+};
+
+export default Card;
